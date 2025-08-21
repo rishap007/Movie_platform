5521aa89ac856af2a06bd7ad1355e639dc171b3b
rishap007 <rishapsharma62476@gmail.com>
2025-08-21 23:45:01 +0530

    adding search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React,{ useEffect, useState } from "react";
 import Search from "./components/Search";
 import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';  
 
@@ -19,13 +20,18 @@ const App = () => {
   const [moviesList, setMoviesList] = useState([]);
   const[isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
-  const fetchMovies = async () => {
+  useDebounce( () => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response= await fetch(endpoint, API_OPTIONS);
 
@@ -52,8 +58,8 @@ const App = () => {
   }
 
   useEffect( () =>{
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return(
     <>
